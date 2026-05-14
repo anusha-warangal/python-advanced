c8aaf9fa99c7c7f4759b37bbd21fce1e876af63c
anusha <ellaboinabikshapathi@gmail.com>
2026-05-14 12:05:07 +0530

    Mark this as day 1 of python

--- for -else.py
+++
@@ -1,8 +0,0 @@
-# Day 1 - for...else prime checker
-num = 29
-for i in range(2, num):
-    if num % i == 0:
-        print(f"{num} is NOT prime")
-        break
-else:
-    print(f"{num} is PRIME")  # This runs
---
+++ hello.py
@@ -0,0 +1,1 @@
+print("Hello Anusha - Python Day 1")
