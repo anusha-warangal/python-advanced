7f4357b785ba16652ba1668a7810fece8e102411
anusha <ellaboinabikshapathi@gmail.com>
2026-05-15 15:19:22 +0530

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,16 @@
+# Day 4: Strings and User Input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# f-string = Easy way to mix text + variables
+print(f"Hi {name}, you are {age} years old.")
+
+# String methods - Useful for DA data cleaning
+print(f"Uppercase: {name.upper()}")
+print(f"Name length: {len(name)}")
+
+# DA use: Age check
+if age >= 18:
+    print("You are eligible for DA internship.")
+else:
+    print("Focus on skills. Apply at 18+.")
